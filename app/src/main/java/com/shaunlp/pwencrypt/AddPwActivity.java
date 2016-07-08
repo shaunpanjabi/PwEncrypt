05f9629b2996f7443a7b4995f39e5058bf2faa99
Shaun Panjabi <shaunpanjabi@gmail.com>
2016-07-07 22:21:41 -0700

    Added content provider. AddPwActivity now stores data into content provider and checks if EditText fields are empty or not.

--- app/src/main/java/com/shaunlp/pwencrypt/AddPwActivity.java
+++ app/src/main/java/com/shaunlp/pwencrypt/AddPwActivity.java
@@ -1,7 +1,13 @@
 package com.shaunlp.pwencrypt;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPwActivity extends AppCompatActivity {
 
@@ -9,5 +15,52 @@ public class AddPwActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pw);
+
+        final ContentValues values = new ContentValues();
+        Button saveButton = (Button) findViewById(R.id.save_button);
+        final EditText descField = (EditText) findViewById(R.id.editDescriptionText);
+        final EditText usernameField = (EditText) findViewById(R.id.editUsernameText);
+        final EditText pwField = (EditText) findViewById(R.id.editPwText);
+        final EditText sourceField = (EditText) findViewById(R.id.editSourceText);
+        final EditText notesField = (EditText) findViewById(R.id.editNotesText);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String descTxt = descField.getText().toString();
+                String usernameTxt = usernameField.getText().toString();
+                String pwTxt = pwField.getText().toString();
+                String sourceTxt = sourceField.getText().toString();
+                String notesTxt = notesField.getText().toString();
+                boolean proceed = true;
+                if (TextUtils.isEmpty(descTxt)){
+                    descField.setError("Description cannot be empty.");
+                    proceed = false;
+                }
+                if (TextUtils.isEmpty(usernameTxt)){
+                    usernameField.setError("Username cannot be empty.");
+                    proceed = false;
+                }
+                if (TextUtils.isEmpty(pwTxt)){
+                    pwField.setError("Password cannot be empty.");
+                    proceed = false;
+                }
+
+                if (proceed) {
+                    // Todo: Encrypt data before adding
+                    values.put(PwDataProvider.description, descTxt);
+                    values.put(PwDataProvider.username, usernameTxt);
+                    values.put(PwDataProvider.password, pwTxt);
+                    if (!TextUtils.isEmpty(sourceTxt)){
+                        values.put(PwDataProvider.source, sourceTxt);
+                    }
+                    if (!TextUtils.isEmpty(notesTxt)){
+                        values.put(PwDataProvider.notes, notesTxt);
+                    }
+                    Toast.makeText(getBaseContext(), "Data was successfully saved", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/shaunlp/pwencrypt/PwDataProvider.java
@@ -0,0 +1,163 @@
+package com.shaunlp.pwencrypt;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+/**
+ * Created by shaunpanjabi on 7/6/16.
+ */
+public class PwDataProvider extends ContentProvider{
+
+    private SQLiteDatabase sqlDb;
+    static final String DATABASE_NAME = "pwdEncryptDB";
+    static final int DATABASE_VERSION = 1;
+    static final String TABLE_NAME = "pwds";
+    static final String CREATE_TABLE =
+            " CREATE TABLE " + TABLE_NAME +
+            " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            " description TEXT NOT NULL, " +
+            " username TEXT NOT NULL, " +
+            " pw TEXT NOT NULL, " +
+            " source TEXT DEFAULT NULL, " +
+            " notes TEXT DEFAULT NULL);";
+
+    static final String id = "id";
+    static final String description = "description";
+    static final String username = "username";
+    static final String password = "pw";
+    static final String source = "source";
+    static final String notes = "notes";
+
+    static final String PROVIDER_NAME = "com.shaunlp.sandbox.PwDataProvider";
+    static final String URL = "content://" + PROVIDER_NAME + "/pwds";
+    static final Uri CONTENT_URL =  Uri.parse(URL);
+
+    private static HashMap<String, String> values;
+
+    static final int uriCode = 1;
+    static final UriMatcher uriMatcher;
+
+    static {
+        uriMatcher  = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "pwds", uriCode);
+    }
+
+    @Override
+    public boolean onCreate() {
+        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
+        sqlDb = dbHelper.getWritableDatabase();
+
+        if (sqlDb != null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(TABLE_NAME);
+
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                queryBuilder.setProjectionMap(values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported URI" + uri);
+        }
+
+        Cursor cursor = queryBuilder.query(sqlDb, projection, selection, selectionArgs, null, null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)){
+            case uriCode:
+                return "vnd.android.cursor.dir/pwds";
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+
+        long rowId = sqlDb.insert(TABLE_NAME, null, values);
+
+        if(rowId > 0){
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URL, rowId);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        } else {
+            Toast.makeText(getContext(), "Row Insert Failed",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int rowsDeleted = 0;
+
+        switch (uriMatcher.match(uri)){
+            case uriCode:
+                rowsDeleted = sqlDb.delete(TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int rowsUpdated = 0;
+
+        switch (uriMatcher.match(uri)){
+            case uriCode:
+                rowsUpdated = sqlDb.update(TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return rowsUpdated;
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper{
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+}
